9a5ab8abcc6f37d0ac60798d903cc5c41a7a3ae4
Ashley Horton <aehorton@madisoncollege.edu>
2023-04-18 11:34:34 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Student from "./components/Student";
+
+function App() {
+  return (
+    <div className="App">
+      <Student name="Ashley"></Student>
+    </div>
+  );
+}
+
+export default App;
